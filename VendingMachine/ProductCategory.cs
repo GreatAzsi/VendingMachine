5452aa2475f4edf5e9b1eed7be29e8a4500db5e5
GreatAzsi <akospotfiok@gmail.com>
2026-09-21 13:40:04 +0200

    Added Product Class

---
+++ VendingMachine/ProductCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public class ProductCategory
+    {
+        public enum productCategory
+        {
+            Drinks, Snack, Food
+        }
+    }
+    
+}
